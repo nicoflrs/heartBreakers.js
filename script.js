3cc913a952e1c6e6ab1e8a6194cc4bfddbcd1139
Nico <floresni1996@gmail.com>
2023-02-15 19:53:14 -0800

    HEART EHART HEART

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ window.addEventListener("click", displayHeart);
 function displayHeart(e) {
   console.log(e)
   const newHeartImage = document.createElement('img');
-  newHeartImage.src = 'assets/valentinesCandyHeartsTwo.png'
+  newHeartImage.src = chrome.runtime.getURL("assets/valentinesCandyHeartsTwo.png");
   document.body.appendChild(newHeartImage);
 }
