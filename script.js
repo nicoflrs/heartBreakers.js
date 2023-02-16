403f9d7f797ec7ce3954221f357641351061ca9c
Katrina <henderson.katrina33@gmail.com>
2023-02-15 18:40:41 -0800

    added button to popup

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+const button = document.createElement('button');
+button.setAttribute('name', 'test button');
+document.body.appendChild(button);
+
+
+function displayHeart(){
+
+}
