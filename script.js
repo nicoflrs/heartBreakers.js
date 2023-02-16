dc152d45213cb1907e1207e65e29432b44da0967
Katrina <henderson.katrina33@gmail.com>
2023-02-15 21:06:55 -0800

    added arrow

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 
+window.addEventListener("click", arrow);
 
-
-window.addEventListener("click", displayHeart);
 
 function displayHeart(e) {
+  let clicked  = `left:${e.offsetX}px;top:${e.offsetY}px`;
   console.log(e)
   const newHeartImage = document.createElement('img');
   newHeartImage.src = chrome.runtime.getURL("assets/valentinesCandyHeartsTwo.png");
-  newHeartImage.style = `left:${e.offsetX}px;top:${e.offsetY}px`
+  newHeartImage.style = `left:${e.offsetX}px;top:${e.offsetY}px`;
   document.body.appendChild(newHeartImage);
+};
+
+function arrow (e) {
+  console.log('hello from arrow');
+  let clicked  = `left:${e.offsetX}px;top:${e.offsetY}px`;
+  const arrowImage = document.createElement('img');
+  arrowImage.setAttribute('class', 'arrowImage');
+  arrowImage.src = chrome.runtime.getURL('assets/arrow.png');
+  arrow.style = clicked;
+  document.body.appendChild(arrowImage);
 }
