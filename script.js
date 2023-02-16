1ab90282c99ca43c99a863d8312a94b1c29b80cd
Katrina <henderson.katrina33@gmail.com>
2023-02-15 21:34:01 -0800

    keyframes taken out of css file and into script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 window.addEventListener("click", arrow);
 
-
 function displayHeart(e) {
   const newHeartImage = document.createElement('img');
   newHeartImage.src = chrome.runtime.getURL("assets/valentinesCandyHeartsTwo.png");
@@ -16,9 +15,24 @@ function displayHeart(e) {
 function arrow (e) {
   console.log('hello from arrow');
   let clicked  = `left:${e.offsetX}px;top:${e.offsetY}px`;
+  console.log('clicked:', clicked);
+  const shootArrow = [
+    // from
+    {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+    },
+    // to
+    {
+      top: `${e.clientY} - 50px`,
+      left: `${e.clientX} - 50px`,
+    }
+  ]
   const arrowImage = document.createElement('img');
   arrowImage.setAttribute('class', 'arrowImage');
   arrowImage.src = chrome.runtime.getURL('assets/arrow.png');
-  arrow.style = clicked;
+  arrowImage.style = clicked;
+  arrowImage.animate(shootArrow, 3000);
   document.body.appendChild(arrowImage);
 }
